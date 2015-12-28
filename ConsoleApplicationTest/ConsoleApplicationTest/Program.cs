ab67a47537d58023c6a012ae6181eb036fa6b718
Sahil Sharma <sahil.sharma@carwale.com>
2015-12-28 18:22:16 +0530

    first checkin

--- ConsoleApplicationTest/ConsoleApplicationTest/Program.cs
+++ ConsoleApplicationTest/ConsoleApplicationTest/Program.cs
@@ -3,14 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace BasicApp
+namespace ConsoleApplicationTest
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("This is first commit");
-            Console.WriteLine("This is first developer's change.");
         }
     }
 }
